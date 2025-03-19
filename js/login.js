583b7b570aeeb9c006de3eab7231f5be06bda7be
sifayet007 <sifayet2004@gmail.comm>
2025-03-19 13:24:59 +0600

    index file update

--- js/login.js
+++ js/login.js
@@ -1,1 +1,4 @@
-console.log("clicked button");
+// console.log("clicked button");
+document.getElementById("login-btn").addEventListener("click", function () {
+  console.log("login button clicked");
+});
