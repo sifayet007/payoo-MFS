c396cc1ccdfc11796e34a704b85b4e5996ce15c3
sifayet007 <sifayet2004@gmail.comm>
2025-03-20 10:08:20 +0600

    added home section

--- js/login.js
+++ js/login.js
@@ -1,12 +1,34 @@
 // console.log("clicked button");
 //search : form submit reloading the page
 // step 1: set event handlers
+// document
+//   .getElementById("login-btn")
+//   .addEventListener("click", function (event) {
+//     //step 2: prevent default behavior (prevent reloading browser)
+//     event.preventDefault();
+//     //get phone number and pin
+//     const phoneNumber = document.getElementById("phone-number").value;
+//     const pinNumber = document.getElementById("pin-number").value;
+//     // console.log(phoneNumber, pinNumber);
+//     if (phoneNumber === "01890727967" && pinNumber === "5588") {
+//       console.log("you are logged in");
+//       window.location.href = "home.html";
+//     } else {
+//       alert("wrong phone number or pin.");
+//     }
+//   });
+
 document
   .getElementById("login-btn")
   .addEventListener("click", function (event) {
-    //step 2: prevent default behavior (prevent reloading browser)
     event.preventDefault();
-    console.log("login button clicked");
+
     const phoneNumber = document.getElementById("phone-number").value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById("pin-number").value;
+    if (phoneNumber === "01890727967" && pinNumber === "5588") {
+      console.log("you are logged in");
+      window.location.href = "home.html";
+    } else {
+      alert("wrong phone number or pin");
+    }
   });
---
+++ js/login1.js
@@ -0,0 +1,8 @@
+// document
+//   .getElementById("login-btn")
+//   .addEventListener("click", function (event) {
+//     event.preventDefault();
+//     const phoneNumber = document.getElementById("phone-number").value;
+//     const pinNumber = document.getElementById("pin-number").value;
+//     console.log(phoneNumber, pinNumber);
+//   });
