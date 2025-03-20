fe7427370c45fa9bba99b239186660dffe036369
sifayet007 <sifayet2004@gmail.comm>
2025-03-20 11:53:44 +0600

    adding money to acount

--- js/home.js
+++ js/home.js
@@ -4,5 +4,16 @@ document
     event.preventDefault();
     const inputMoney = document.getElementById("input-money").value;
     const inputPin = document.getElementById("input-pin").value;
-    console.log(inputMoney, inputPin);
+    if (inputPin === "5588") {
+      console.log("adding money to your account");
+      const balance = document.getElementById("account-balance").innerText;
+
+      const addInputBalance = parseFloat(balance);
+      const addInputMoney = parseFloat(inputMoney);
+      const newBalance = addInputBalance + addInputMoney;
+      console.log(newBalance);
+      document.getElementById("account-balance").innerText = newBalance;
+    } else {
+      alert("Failed to add money please try again");
+    }
   });
