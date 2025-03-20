e445477b220f2e9c40e7aaaba726471d98ddc2ef
sifayet007 <sifayet2004@gmail.comm>
2025-03-20 14:20:07 +0600

    added a addmoney mathor

--- js/addMoney.js
+++ js/addMoney.js
@@ -3,12 +3,16 @@ document
   .addEventListener("click", function (event) {
     event.preventDefault();
     const inputMoney = document.getElementById("input-money").value;
-    const addInputMoney = parseFloat(inputMoney);
     const inputPin = document.getElementById("input-pin").value;
     if (inputPin === "5588") {
       const balance = document.getElementById("account-balance").innerText;
+
+      const addInputMoney = parseFloat(inputMoney);
       const addInputBalance = parseFloat(balance);
       const newBalance = addInputMoney + addInputBalance;
+
       document.getElementById("account-balance").innerText = newBalance;
+    } else {
+      alert("Failed to add money please try again");
     }
   });
